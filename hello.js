70a4c0e4cf3d398c3fcd5ae35d500796a8d245ab
AnirudhaPatil-1 <aninp2020@gmail.com>
2023-08-12 16:42:56 +0530

    modular code - learn imports and exports - module.exports & require

--- hello.js
+++ hello.js
@@ -1,2 +1,28 @@
-console.log("Hello world!");
-console.log("Hello world!");
+// console.log("Hello world!");
+
+//custom script -> npm scriptName
+
+// function add (value1, value2){
+//     return value1 + value2;
+// };
+
+const math = require('./math.js');
+
+// // console.log(add); //Anirudha
+
+// // console.log(add(2, 3));
+// console.log(math); 
+// // O/P -> { addFn: [Function: add], subtractFn: [Function: subtract] }
+
+// console.log("add function: " + math.addFn(2, 3));
+// console.log("subtract function: " + math.substractFn(2, 3));
+
+// const {add, substract} = require("./math");
+// console.log(add(2,3));
+// console.log(substract(2,3));
+
+
+console.log("math: " + math);
+
+const fs = require('fs');
+console.log(fs);
---
+++ math.js
@@ -0,0 +1,31 @@
+function add(value1, value2){
+    return value1 + value2;
+}
+
+function substract (value1, value2){
+    return value1 - value2;
+}
+
+// module.exports = "Anirudha";
+
+// module.exports = add;
+// module.exports = subtract;
+// module.exports = {
+//     addFn : add,
+//     substractFn: substract,
+// }
+
+module.exports = {
+    add,
+    substract
+}
+
+// exports.add = (val1, val2) => {
+//     return val1 + val2;
+// }
+
+// exports.sub = (val1, val2) => {
+//     return val1 - val2;
+// }
+
+
